e1330b62de7edbc0e6060b2a5c6b199902c3e8bb
butdory <butdory04@gmail.com>
2021-07-20 13:22:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 2021 선린인터넷 고등학교 웹 소수전공
+
+## React를 이용한 op.gg 웹 사이트 클론코딩
+"# op.gg_cloneproject" 
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ChampionsList from './pages/ChampionsList';
+import {createGlobalStyle} from 'styled-components';
+import Header from './components/Header';
+
+const MyGlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: rgba(0,0,0,.1);
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`
+
+
+function App() {
+  return (
+    <>
+      <MyGlobalStyle />
+      <Header headerHeight={100}/>
+      <ChampionsList />
+    </>
+  );
+}
+
+export default App;
